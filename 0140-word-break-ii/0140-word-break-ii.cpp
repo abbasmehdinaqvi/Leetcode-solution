5ea9f989f0197b5d629512f68de6111db8642fb4
abbasmehdinaqvi <75295906+abbasmehdinaqvi@users.noreply.github.com>
2022-11-14 14:18:03 +0530

    Time: 5 ms (22.11%), Space: 7.5 MB (27.04%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    void solve(string &s, unordered_set<string> &q, int start, int end, string str, string word, vector<string> &ans)
+    {
+        if(end >= s.size())
+        {
+            if(start >= s.size())
+            {
+                ans.push_back(str);
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+        
+        word = s.substr(start, end - start + 1);
+        
+        if(q.count(word) && word.size() > 0)
+        {
+            if(str.size() == 0)
+                solve(s, q, end + 1, end +1, str + word, "", ans);
+                
+            else    
+                solve(s, q, end + 1, end +1, str + " " + word, "", ans);
+        }
+        
+            solve(s, q, start, end +1, str, word, ans);
+    }
+    
+    
+    
+    
+    
+    
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        
+        unordered_set<string> q(wordDict.begin(), wordDict.end());
+        
+        vector<string> ans;
+        
+        solve(s, q, 0, 0, "", "", ans);
+      
+        return ans;
+    }
+};
